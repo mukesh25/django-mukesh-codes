7acbff159ef4929fbe6a1322174427de5b24f856
Mukesh Kumar <poddarm85@gmail.com>
2020-09-27 21:45:47 +0530

    changes in mywebsite

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*=px!g(om^x37y%157@v=h@(vho=v*2()2e5=-uhyf6fim=hw#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mukeshpoddar.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,8 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT=os.path.join(BASE_DIR,'static/')
